3649b4d4c471166f0a00b0b979e4aff63146cb65
Laurent Maumet <laurent.maumet@gmail.com>
2020-07-05 17:29:34 +0200

    date

--- TheTempProject.ino
+++ TheTempProject.ino
@@ -5,8 +5,9 @@
 #include <ESPAsyncWebServer.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <ArduinoOTA.h>
 
-#include <NetworkCredentials.h> // This fileis in the <Arduino>/libraries/MyCommon directory defines SSID : the ssid of the wifi network , PASSWORD : the network's password.
+#include <NetworkCredentials.h> // This file is in the <Arduino>/libraries/MyCommon directory defines SSID : the ssid of the wifi network , PASSWORD : the network's password.
 
 #define DHTPIN D1 // Digital pin connected to the DHT sensor
 
@@ -113,7 +114,8 @@ void setup()
   // Serial port for debugging purposes
   Serial.begin(115200);
   dht.begin();
-/*
+  // Hostname defaults to esp8266-[ChipID]
+
   // Connect to Wi-Fi
   WiFi.begin(SSID, PASSWORD);
   Serial.println("Connecting to WiFi");
@@ -122,7 +124,36 @@ void setup()
     delay(1000);
     Serial.println(".");
   }
-  */
+
+  ArduinoOTA.setHostname(HOSTNAME);
+  WiFi.hostname(HOSTNAME);
+  ArduinoOTA.setPassword(PASSWORD);
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   // Print ESP8266 Local IP Address
   Serial.println(WiFi.localIP());
@@ -176,4 +207,5 @@ void loop()
       Serial.println(h);
     }
   }
+  ArduinoOTA.handle();
 }
